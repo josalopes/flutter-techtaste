ed5006dc48d89462c74ae11acc039ab38f8ae381
Francisco Josafá <josafa.lopes@inventione.com.br>
2025-05-10 12:07:04 -0300

    Cria tela de restaurantes e carrinho de compras

---
+++ lib/data/restaurants_data.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+import 'package:myapp/model/restaurant.dart';
+
+class RestaurantData extends ChangeNotifier {
+  List<Restaurant> listRestaurant = [];
+  Future<void> getRestaurants() async {
+    String jsonString = await rootBundle.loadString('assets/data.json');
+    Map<String, dynamic> data = await json.decode(jsonString);
+
+    List<dynamic> restaurants = data['restaurants'];
+
+    for (var restaurantData in restaurants) {
+      listRestaurant.add(Restaurant.fromMap(restaurantData));
+    }
+  }
+}
---
+++ lib/ui/_core/bag_provider.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/model/dish.dart';
+
+class BagProvider extends ChangeNotifier {
+  List<Dish> dishesOnBag = [];
+
+  addAllDishes(List<Dish> dishes) {
+    dishesOnBag.addAll(dishes);
+    notifyListeners();
+  }
+
+  removeDish(Dish dish) {
+    dishesOnBag.remove(dish);
+    notifyListeners();
+  }
+
+  clearBag() {
+    dishesOnBag.clear();
+    notifyListeners();
+  }
+
+  double getTotalPrice() {
+    double totalPrice = 0.0;
+    for (Dish dish in dishesOnBag) {
+      totalPrice += dish.price;
+    }
+
+    return totalPrice;
+  }
+
+  Map<Dish, int> getMapByAmount() {
+    Map<Dish, int> mapResult = {};
+
+    for (Dish dish in dishesOnBag) {
+      if (mapResult[dish] == null) {
+        mapResult[dish] = 1;
+      } else {
+        mapResult[dish] = mapResult[dish]! + 1;
+      }
+    }
+
+    return mapResult;
+  }
+}
